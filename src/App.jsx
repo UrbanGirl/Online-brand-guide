328917e2c43f79d8c8fb4cf31139cb62ede80a99
Cuntasourous <maram345000@gmail.com>
2026-05-02 01:44:10 +0300

    logo section in progress

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
 
 function LoadingScreen(){
   return (
-    <div className='center-middle-container'>
+    <div className='loading-container'>
       <div class="arrow">
           <span></span>
           <span></span>
@@ -67,10 +67,10 @@ function NavigationBar() {
       </div>
 
       <div className="links">
-        <a href='#'>Logo</a>
+        <a href='#Logo'>Logo</a>
         <a href='#'>Graphics</a>
         <a href='#'>Typography</a>
-        {/* <a href='#'>Iconography</a> */}
+        <a href='#'>Stationary</a>
       </div>
 
       <div className="actions">
@@ -105,27 +105,34 @@ function Header(){
 }
 
 function LogoSection(){
+  // const {Lightmode, setLightmode} = useState(false);
+
+
   return(
-    <div className='Logo-section'>
-      <div className='logo-description flex'>
-        <h2>Metaphor and symbol</h2>
-        <p>The logo metaphor is based on «slash» — one of the basic elements of an Internet address. It works as a symbol of nesting, denoting the strategy of Rambler.ru as a single portal on whose services you can find solutions to any issues.</p>
-      </div>
-      <div className='logo-image'>
-        <div className='current-logo'></div>
-      </div>
-      <div className='logo-ctas flex'>
-        <a href='#'>Download Logo</a>
-        <a href='#'>Download SVG</a>
-        <a href='#'>Download PNG</a>
-      </div>
-      <div className='System-modes'>
-        <a href='#'>
-          Black-mode
-        </a>
-        <a href='#'>
-          Light mode
-        </a>
+    <div className='Logo-container' id='Logo'>
+      <div className='Logo-section'>
+        <div className='flex'>
+          <div className='logo-description'>
+            <h1>Metaphor and symbol</h1>
+            <p>The logo metaphor is based on «slash» — one of the basic elements of an Internet address. It works as a symbol of nesting, denoting the strategy of Rambler.ru as a single portal on whose services you can find solutions to any issues.</p>
+          </div>
+          <div className='System-modes'>
+            <a href='#'>
+              <div className='box box-white '></div>
+            </a>
+            <a href='#'>
+              <div className='box box-black selected-mode'></div>
+            </a>
+          </div>
+        </div>
+        <div className='logo-image'>
+          <img src={Logo} alt='white-mode-logo' />
+        </div>
+        <div className='logo-ctas flex'>
+          <p>Download Logo</p>
+          <a href='#'><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#7E529B" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-vector-square-icon lucide-vector-square"><path d="M19.5 7a24 24 0 0 1 0 10"/><path d="M4.5 7a24 24 0 0 0 0 10"/><path d="M7 19.5a24 24 0 0 0 10 0"/><path d="M7 4.5a24 24 0 0 1 10 0"/><rect x="17" y="17" width="5" height="5" rx="1"/><rect x="17" y="2" width="5" height="5" rx="1"/><rect x="2" y="17" width="5" height="5" rx="1"/><rect x="2" y="2" width="5" height="5" rx="1"/></svg></a>
+          <a href='#'><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#7E529B" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-image-icon lucide-image"><rect width="18" height="18" x="3" y="3" rx="2" ry="2"/><circle cx="9" cy="9" r="2"/><path d="m21 15-3.086-3.086a2 2 0 0 0-2.828 0L6 21"/></svg></a>
+        </div>
       </div>
     </div>
   )
